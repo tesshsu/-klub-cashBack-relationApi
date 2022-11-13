9266df5fe2e7699c25d4f98c3a358401176a38c8
DESKTOP-3776QB7\Livmeds10 <yating@livmeds.com>
2022-11-13 10:22:48 +0100

    Updated transaction by cb payment join with merchant information enpoint

--- src/model/Transaction.js
+++ src/model/Transaction.js
@@ -2,6 +2,7 @@ const knex = require('knex')
 const config = require('../../knexfile')
 const AmountUnit = require("./Transaction");
 const db = knex(config.development)
+const Account = require("./Account");
 
 class Transaction{
 
@@ -18,7 +19,7 @@ class Transaction{
             dbTransaction.transaction_id = transaction.transactionId;
             dbTransaction.account_id = transaction.accountId;
 
-            if(transaction.unit.toLowerCase() === Transaction.AMOUNT_UNIT_SINGLE.toLowerCase()){
+            if(transaction.unit && transaction.unit.toLowerCase() === Transaction.AMOUNT_UNIT_SINGLE.toLowerCase()){
                 dbTransaction.amount = transaction.amount * 100;
             } else{
                 dbTransaction.amount = transaction.amount;
@@ -38,48 +39,16 @@ class Transaction{
     static validate(transaction) {
         let valid = true
         if (!transaction.amount) valid = false
-        if (!transaction.unit) valid = false
         if (!transaction.type) valid = false
         if (!transaction.transactionId) valid = false
         if (!transaction.accountId) valid = false
         return valid
     }
 
-
-    // tested
-    static async getAll() {
-        return await db.select().table('transaction')
-    }
-
-    // tested
-    static async getOne(id) {
-        const transaction = await db('transaction').where({id})
-
-        /**
-         * In case of wish not found, knex returns an empty array.
-         * Otherwise, it returns an array with just one item inside,
-         * which would be the wish we're looking for.
-         */
-         if (transaction.length === 0) return undefined
-         return transaction[0]
-    }
-
-    static async getAllOfOneUser(accountId) {
-        return await db('transactions').where({ account_id: accountId })
-    }
-
-    // tested
-    static async patch(id, patches) {
-        const transactionToPatch = {}
-
-        if (patches.type) transactionToPatch.type = patches.type
-        if (patches.total) transactionToPatch.total = patches.total
-        if (patches.extra) transactionToPatch.extra = patches.extra
-        const patch = await db('accounts').where({id}).update(accountToPatch, ['id', 'type', 'total', 'extra'])
-
-        if (patch > 0) return { message: 'Transaction updated successfully!'}
-        return undefined
-
+    static async getAllOfOneUser(userId) {
+        const userAccount = await Account.getAllOfOneUser(userId);
+        const userAccountId = userAccount[0].id
+        return db('transactions').where('account_id','=', userAccountId).orderBy('amount', 'desc')
     }
 
     // tested
